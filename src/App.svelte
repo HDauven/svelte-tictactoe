46a0a20c5a60bf20c92e19ae9302dd5e5e2cd883
Hein Dauven <heindauven@gmail.com>
2021-03-12 16:56:41 +0100

    Add game layout.

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,36 @@
 <script>
   import Space from "./Space.svelte";
+  import gameStore from "./game-store.js";
+  import { nextMove, reset } from "./requests.js";
+
+  let board = ["", "", "", "", "", "", "", "", ""];
+  let nextPlayer = "";
+  let winner;
+  let numberOfPeeps = 0;
+  let errorMessage;
+
+  gameStore.subscribe(state => {
+    if (!state) {
+      return;
+    }
+
+    winner = state.winner;
+    nextPlayer = state.nextPlayer;
+    board = state.board;
+    numberOfPeeps = state.numberOfPeeps;
+  });
+
+  async function takeSpace(space) {
+    if (winner || !gameStore.isConnected) {
+      return;
+    }
+
+    errorMessage = await nextMove(space);
+  }
+
+  async function newGame() {
+    errorMessage = await reset();
+  }
 </script>
 
 <style>
@@ -25,30 +56,47 @@
   button:hover {
     outline: none;
   }
+
+  .errorMessage {
+    color: red;
+    font-size: 20px;
+  }
 </style>
 
 <main>
   <h1>Tic Tac Toe</h1>
-  <h2>Number of people playing:</h2>
-  <h2>Player X</h2>
+  <h2>Number of people playing: {numberOfPeeps}</h2>
+  {#if winner == 'TIE'}
+    <h2>Tie!</h2>
+  {:else if winner}
+    <h2>Player {winner} won!</h2>
+  {:else}
+    <h2>Player {nextPlayer}</h2>
+  {/if}
 
   <div class="row">
-    <Space space="X" />
-    <Space space="X" />
-    <Space space="X" />
+    <Space {winner} space={board[0]} on:click={() => takeSpace(0)} />
+    <Space {winner} space={board[1]} on:click={() => takeSpace(1)} />
+    <Space {winner} space={board[2]} on:click={() => takeSpace(2)} />
   </div>
 
   <div class="row">
-    <Space space="X" />
-    <Space space="X" />
-    <Space space="X" />
+    <Space {winner} space={board[3]} on:click={() => takeSpace(3)} />
+    <Space {winner} space={board[4]} on:click={() => takeSpace(4)} />
+    <Space {winner} space={board[5]} on:click={() => takeSpace(5)} />
   </div>
 
   <div class="row">
-    <Space space="X" />
-    <Space space="X" />
-    <Space space="X" />
+    <Space {winner} space={board[6]} on:click={() => takeSpace(6)} />
+    <Space {winner} space={board[7]} on:click={() => takeSpace(7)} />
+    <Space {winner} space={board[8]} on:click={() => takeSpace(8)} />
   </div>
 
-  <button>New Game</button>
+  {#if winner}
+    <button on:click={newGame}>New Game</button>
+  {/if}
+
+  {#if errorMessage}
+    <p class="errorMessage">{errorMessage}</p>
+  {/if}
 </main>
